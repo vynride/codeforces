3b92db0e7062ae59a625caf67c1ae7be47e6b500
vynride <vynride@gmail.com>
2024-09-17 22:45:58 +0530

    clear up 4A watermelon solution

--- div2 A/4A-watermelon.c
+++ div2 A/4A-watermelon.c
@@ -5,7 +5,7 @@ int main(void)
     int w;
     scanf("%d", &w);
 
-    // 2 is the only even number (apart from 0) that cannot be split into two even numbers 
+    // 2 is the only even number that cannot be split into two even numbers (not necessarily equal) 
     // E.g. 50 can be split as 24 and 26
     if (w > 2)
     {
